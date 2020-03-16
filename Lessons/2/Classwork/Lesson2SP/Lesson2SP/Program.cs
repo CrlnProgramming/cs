dbc16943c2c906e958b38fd34e57984755ee0aa0
--global <--global>
2020-03-16 22:13:20 +0300

    ASD

---
+++ Lessons/2/Classwork/Lesson2SP/Lesson2SP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2SP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            decimal number = decimal.Parse(line);
+        }
+    }
+}
+
+/* int a = 5;
+            short b = 2;
+            float f1 = 10.5f;
+            decimal dec1 = 32M;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(f1);
+            Console.WriteLine(dec1);
+            */
