3c25528ac0259d541dbcbaed953689bfd91164d2
Ilya <CrlnPro@yandex.ru>
2020-03-26 16:20:12 +0300

    lessons4NW

---
+++ Lessons/4/Homework/Homework/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Homework
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var names = new string[3]; 
+            var old = new int[3];
+            for (var i = 0; i < names.Length; i++)
+            {
+                Console.Write($"Введите Имя №{i}: ");
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            for (var j = 0; j < old.Length; j++)
+            {
+                Console.Write($"Введите возраст {j}: ");
+                old[j] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for (var i = 0; i < names.Length;)
+            {
+                Console.WriteLine(names[i]);
+                i++;
+            }
+            Console.WriteLine();
+            for (var j = 0; j <names.Length; )
+            {
+                Console.WriteLine($"{names[j]} Возраст через 4 года :{old[j] + 4} ");
+                j++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ Lessons/5/Homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    enum Container:byte
+    {
+        Litter = 0b00000001,
+        FiveLitter = 0b00000010,
+        TwentyLitter = 0b00000100,//20
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write ur number: ");
+            var number = Convert.ToDouble(Console.ReadLine());
+            
+
+            if (number >= 20)
+            {
+                var number20 = Convert.ToInt32(number % 20);
+                number20 = number20 | (int)Container.TwentyLitter;
+                if ((number20 & (int)Container.TwentyLitter) == (int)Container.TwentyLitter)
+                {
+                    Console.WriteLine($"В числе присутствует контейнеры больше 20 литров {Container.TwentyLitter}");
+                }
+            }
+
+            if (number <= 5)
+            {
+                var number1 = Convert.ToInt32(number % 5); number1 = number1 | (int)Container.Litter;
+                if ((number1 & (int)Container.FiveLitter) == (int)Container.FiveLitter)
+                {
+                    Console.WriteLine("В числе присутствует контейнеры по 5 литров");
+                }
+            }
+
+            if (number > 5 & number < 20)
+            {
+                var number5 = Convert.ToInt32(number % 5); number5 = number5 | (int)Container.FiveLitter;
+                if ((number5 & (int)Container.FiveLitter) == (int)Container.FiveLitter)
+                {
+                    Console.WriteLine("В числе присутствует контейнеры по 5 литров");
+                }
+            }
+        }
+    }
+}
+
+
